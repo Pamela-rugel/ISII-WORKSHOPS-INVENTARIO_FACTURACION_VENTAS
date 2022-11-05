d17e5cb782dad7fad45a84768ff7900ff422ef05
PamelaRugel <parudiaz@espol.edu.ec>
2022-11-05 00:17:29 -0500

    "final acceptance testing"

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
@@ -12,7 +12,7 @@ public class StepsDefs {
  
  //1
  
- @Given("The customer has 50 years old, is single and female, and her license is valid.")
+ @Given("The customer has 50 years old, is single and female, and her license is valid")
  public void customer_has_50years() {
 	 carInsurance.setAge(50);
 	 carInsurance.setSex('F');
@@ -22,36 +22,39 @@ public class StepsDefs {
  }
  
  @When("We Calculate the base premium car insurance")
- public void calculate_baseprenium_() {
- actualAnswer = (int) carInsurance.calcularBase();
+ public void calculate_base_premium_1() {
+	 actualAnswer = (int) carInsurance.calcularBase();
  }
  
- @Then("I should be told ")
- public void i_should_be_told() {
- assertEquals(200, actualAnswer);
+ @Then("I should be told 200")
+ public void i_should_be_told_1() {
+	 assertEquals(200, actualAnswer);
  }
- 
+
  //2
  
- @Given("The customer has 86 years old, is single and female, and her license is valid.")
+ @Given("The customer has 86 years old, is single and female, and her license is valid")
  public void customer_has_86years() {
 	 carInsurance.setAge(86);
+	 carInsurance.setSex('F');
+	 carInsurance.setmaritalStatus(false);
+	 carInsurance.setvalidLicense(true);
 	 
  }
  
  @When("The customer gives her age")
- public void calculatebaseprenium_insurance() {
- actualAnswer = carInsurance.validarEdad(String.valueOf(carInsurance.getAge()));
+ public void calculate_base_premium_2() {
+	 actualAnswer = (int) carInsurance.calcularBase();
  }
  
- @Then("I should be told ")
- public void should_be_told() {
- assertEquals(-1, actualAnswer);
+ @Then("I should be told -1")
+ public void  i_should_be_told_2() {
+	 assertEquals(-1, actualAnswer);
  }
  
  //3
  
- @Given("The customer has 28 years old, is single and male, and his license is valid.")
+ @Given("The customer has 28 years old, is single and male, and his license is valid")
  public void customer_has_28years() {
 	 carInsurance.setAge(28);
 	 carInsurance.setSex('M');
@@ -59,22 +62,34 @@ public class StepsDefs {
 	 carInsurance.setvalidLicense(true);
  }
  
- @When("We Calculate the base premium car insurance")
- public void calculate_baseprenium_insurance() {
- actualAnswer = (int) carInsurance.calcularBase();
- }
- @Then("I should be told ")
- public void i_shouldbe_told() {
- assertEquals(300, actualAnswer);
+ @When("The customer Calculates the premium car insurance")
+ public void calculate_base_premium_3() {
+	 actualAnswer = (int) carInsurance.calcularBase();
  }
  
+ @Then("I should be told 500")
+ public void  i_should_be_told_3() {
+	 assertEquals(500, actualAnswer);
+ }
  
+ //4
  
+ @Given("The customer has \"{int}\" years old, is married and female, and her license is valid")
+ public void customer_has_45_65_years(int age) {
+	 carInsurance.setAge(age);
+	 carInsurance.setSex('M');
+	 carInsurance.setmaritalStatus(true);
+	 carInsurance.setvalidLicense(true);
+ }
  
+ @When("The customer calculates the premium car insurance")
+ public void calculate_base_premium_4() {
+	 actualAnswer = (int) carInsurance.calcularBase();
+ }
  
- 
- 
- 
- 
- //3
+ @Then("I must be told \"{int}\"")
+ public void  i_should_be_told_4(int expected) {
+	 assertEquals(expected,actualAnswer);
+ }
+
 }
--- ec.edu.espol.workshops/lib/src/test/resources/ec/edu/espol/workshops/calculatePremium.feature
+++ ec.edu.espol.workshops/lib/src/test/resources/ec/edu/espol/workshops/calculatePremium.feature
@@ -1,7 +1,7 @@
 Feature: Calculate the Base Premium Insurance 
 
-	Scenario Outline: The customer is a married Female between the ages of 45 and 65
- 		Given The customer has 50 years old, is single and female, and her license is valid.
+	Scenario: The customer is a married Female between the ages of 45 and 65
+ 		Given The customer has 50 years old, is single and female, and her license is valid
  		When We Calculate the base premium car insurance
  		Then I should be told 200
  	
@@ -13,15 +13,15 @@ Feature: Calculate the Base Premium Insurance
 	Scenario: The customer is a 28-year-old single Male 
 		Given The customer has 28 years old, is single and male, and his license is valid
 		When The customer Calculates the premium car insurance
-		Then I should be told 300
+		Then I should be told 500
+	
+	Scenario Outline: The customer is a age between 45 and 65
+		Given The customer has "<age>" years old, is married and female, and her license is valid
+		When The customer calculates the premium car insurance
+		Then I must be told "<answer>"
 
- 	
- 		
- 	Examples:
- 		| age | answer|
- 		| 44  | 300   |
+	Examples:
+		| age | answer|
  		| 45  | 200   |
  		| 46  | 200   |
- 		| 50  | 200   |
- 		| 65  | 300   |
- 		| 66  | 300   |
+ 		| 64  | 200   |
